c26fb3cfb671b0d604c132a78662bd367da3defc
Shaan Singh <ss5432@outlook.com>
2024-11-24 14:35:34 -0500

    Got the logo.

--- magnum/src/components/SearchBar.jsx
+++ magnum/src/components/SearchBar.jsx
@@ -1,18 +1,20 @@
 import '../styles/SearchBar.css'
 import { useState } from 'react'
-import { Search } from 'lucide-react'
+import { Waypoints } from 'lucide-react'
 
 const SearchBar = () => {
     const [query, setQuery] = useState('')
+
     const handleSubmit = (e) => {
         e.preventDefault()
         console.log('Searching for:', query)
     }
+
     return (
         <div className='search-wrapper'>
             <form onSubmit={handleSubmit}>
                 <div className='search-container'>
-                    <Search className='search-icon' size={20} />
+                    <Waypoints className='network-icon' size={20} />
                     <input
                         type='text'
                         placeholder="search through any digital footprint."
